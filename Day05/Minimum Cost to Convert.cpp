04547613ef1a8dfd6f699eac7094e97e0b342134
Abhishek197088 <abhishek197088@gmail.com>
2026-01-29 15:49:02 +0530

    Valid Palindrome

---
+++ Day05/Minimum Cost to Convert.cpp
@@ -0,0 +1,31 @@
+// 2976. Minimum Cost to Convert String I
+// Intuition
+// Approach
+// Complexity
+// Time complexity:
+// Space complexity:
+// Code
+
+#include<iostream>
+#include<string>
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isalnum(s[i])) {
+                ++i;
+            } else if (!isalnum(s[j])) {
+                --j;
+            } else if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            } else {
+                ++i;
+                --j;
+            }
+        }
+        return true;
+    }
+};
+
