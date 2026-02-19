61e774c381646cbabdd7103414f13e04b2a6c729
Abhishek Kumar <110464076+Abhishek197088@users.noreply.github.com>
2026-02-19 15:07:29 +0530

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,10 @@
+/* heaps -> a heap is a complete binary tree that follows the heap order property 
+- complete Binary tree (cbt) -> all levels are completely filled except the last  level, the last level is fileed from left to right 
+- heap order property -> 1. Max heap -> every parent node is greter than or equal to its children the largest element is always at root
+                           2. Min heap -> every parent node is smaller than or equal to its children the smallest
+
+
+for any node 
+. left child
+. right child 
+parent i/2
